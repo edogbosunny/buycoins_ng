4807261d8547be7f550cac15507b1dc93f7bf1bc
edogbosunny <sunnyedogbo@gmail.com>
2020-03-05 20:35:24 +0100

    configure travis

--- src/service/buy-bitcoins..integration.test.ts
+++ src/service/buy-bitcoins..integration.test.ts
@@ -1,5 +1,7 @@
 import BitcoinService from './buy-bitcoins.service';
 
+jest.mock('axios');
+
 describe('#Bitcoin service', () => {
     it('should test the service function', async () => {
         const response = await BitcoinService.getCurrentBitcoinPrice();
